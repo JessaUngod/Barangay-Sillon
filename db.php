be8be14a038318788cf3fd52099570d5fd927eef
JessaUngod <maeung00@gmail.com>
2024-07-19 15:17:13 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,8 @@
 <?php 
 session_start();
-$con = mysqli_connect('sql12.freesqldatabase.com','sql12720787','vzW8ZgQIuJ','sql12720787');
+$con = mysqli_connect('localhost','root','','at_tendance');
+
+// $con = mysqli_connect('sql12.freesqldatabase.com','sql12720787','vzW8ZgQIuJ','sql12720787');
 if(!$con){
 	die('Conection failed!' . mysqli_connect_error());
 

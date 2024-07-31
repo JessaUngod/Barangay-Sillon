a6caefd9fc72671f1d71b65850f285d9de663f04
JessaUngod <maeung00@gmail.com>
2024-07-31 12:58:06 +0800

    commit

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 session_start();
 // $con = mysqli_connect('localhost','root','','at_tendance');
 
-$con = mysqli_connect('sql12.freesqldatabase.com','sql12720787','vzW8ZgQIuJ','sql12720787');
+$con = mysqli_connect('127.0.0.1','u510162695_sillon','1Sillon_pass','u510162695_sillon');
 if(!$con){
 	die('Conection failed!' . mysqli_connect_error());
 

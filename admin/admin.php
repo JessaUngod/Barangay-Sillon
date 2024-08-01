81bc6e1555412a750fb70518b2f27c023768420f
JessaUngod <maeung00@gmail.com>
2024-08-01 14:52:00 +0800

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -194,7 +194,8 @@ require_once'../db.php';
                                             <th class="fw-bold bg-primary">Age</th>
                                             <th class="fw-bold bg-primary" >Gender</th>
                                             <th class="fw-bold bg-primary" >Username</th>
-                                            <th class="fw-bold bg-primary" >Profile Picture</th>         
+                                            <th class="fw-bold bg-primary" >Profile Picture</th> 
+                                           <th class="fw-bold bg-primary" >Action</th>
                                                                                   
 
                                         </tr>
@@ -211,10 +212,7 @@ require_once'../db.php';
                                          ?>
                                         <tr>
 
-                                            <td><?php echo $row1['fname'];?><a  class="ms-2 float-end" data-toggle ="modal" data-target ="#deleteq-<?php echo $row1['id']; ?>"><i class="fa fa-trash text-danger fs-6"></i></a>
-
-
-                                                <a class="ms-2 float-end" href="update_admin.php?admin_id=<?php echo $row1['id'];?>"><i class="fa fa-edit text-primary fs-6"></i></a></td>
+                                            <td><?php echo $row1['fname'];?></td>
                                             <td><?php echo $row1['lname']; ?></td>
                                             <td><?php echo $row1['mname']; ?></td>
                                             <td><?php echo $row1['age']; ?></td>
@@ -227,6 +225,10 @@ require_once'../db.php';
 
                                                     
                                                 </td>
+                                           <td><a  class="ms-2 float-end" data-toggle ="modal" data-target ="#deleteq-<?php echo $row1['id']; ?>"><i class="fa fa-trash text-danger fs-6"></i></a>
+
+
+                                                <a class="ms-2 float-end" href="update_admin.php?admin_id=<?php echo $row1['id'];?>"><i class="fa fa-edit text-primary fs-6"></i></a></td>
                                             
                                         </tr>
 

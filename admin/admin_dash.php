eff9977b87051d2aa12a745cb5136cac5431aa5d
JessaUngod <maeung00@gmail.com>
2024-08-01 14:38:46 +0800

    Update admin_dash.php

--- admin/admin_dash.php
+++ admin/admin_dash.php
@@ -302,7 +302,7 @@ if(!empty($_SESSION['idadmins'])){
                             <script type="text/javascript">
                                  var options = {
                                 chart: {
-                                    type: 'area'
+                                    type: 'pie'
                                 },
                                 series:[{
                                     name: 'Count',

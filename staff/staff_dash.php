9d1409b041bbae102bd7a4aee7063718824f6a03
JessaUngod <maeung00@gmail.com>
2024-08-01 18:29:13 +0800

    Update staff_dash.php

--- staff/staff_dash.php
+++ staff/staff_dash.php
@@ -373,7 +373,7 @@ error_reporting(0);
 
                       ?>,
                     <?php  date_default_timezone_set("Asia/manila");
-         $time = date('yy-m-d');
+        $timem = date('y-m-d');
 $sql22 = "SELECT * FROM `attendance` WHERE emp_id = '$idem' AND time_in = '$time'";
  $oks22s = mysqli_query($con, $sql22);
 

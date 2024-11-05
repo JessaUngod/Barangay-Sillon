6e745cda2147ace6be226b25b0afdeb9caab0b66
JessaUngod <maeung00@gmail.com>
2024-11-06 00:40:51 +0800

    Update posistion.php

--- admin/posistion.php
+++ admin/posistion.php
@@ -387,16 +387,19 @@ require_once'../db.php';
  <script src="../assets/js/jquery.min.js"></script>
 <script src="../assets/js/bootstrap.bundle.js"></script>
 <script src="../assets/js/mdb.js"></script>
+ <script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.bundle.js"></script>
+<script src="../assets/js/mdb.js"></script>
 <script>
     // Sidebar toggle functionality
     $('.open-btn').on('click', function() {
-        $('#side_nav').addClass('active');  // Add active class to sidebar
-        $('.content').addClass('shift');    // Shift content to the right
+        $('#side_nav').addClass('active');  
+        $('.content').addClass('shift');    
     });
 
     $('.close-btn').on('click', function() {
-        $('#side_nav').removeClass('active');  // Remove active class to close sidebar
-        $('.content').removeClass('shift');    // Reset content positioning
+        $('#side_nav').removeClass('active');  
+        $('.content').removeClass('shift');  
     });
 </script>
 

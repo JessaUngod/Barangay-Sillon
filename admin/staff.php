6fdead39cf71f5af9a9d58579ff348ccbb40a201
JessaUngod <maeung00@gmail.com>
2024-11-29 00:30:54 +0800

    New CHanges

--- admin/staff.php
+++ admin/staff.php
@@ -302,11 +302,11 @@ require_once'../db.php';
    <script src="../assets/js/jquery.min.js"></script>
    <script src="../assets/js/bootstrap.bundle.js"></script>
  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
- <script src="https://www.google.com/recaptcha/api.js?render=6Lc95IwqAAAAAAqgeTiHvRIFCgIE4LsQortunSBT"></script> 
- <script type="text/javascript">
+ <script src="https://www.google.com/recaptcha/api.js?render=6Lc95IwqAAAAAAqgeTiHvRIFCgIE4LsQortunSBT"></script> <!-- Replace with your Site Key -->
+    <script type="text/javascript">
         grecaptcha.ready(function() {
-            grecaptcha.execute('6Lc95IwqAAAAADyRaUf6N7uobXWvSIC-10Ja-Qnd', { action: 'login' }).then(function(token) {
-              
+            grecaptcha.execute('6Lc95IwqAAAAADyRaUf6N7uobXWvSIC-10Ja-Qnd', { action: 'staff_management' }).then(function(token) {
+                // Set the token in a hidden field
                 document.getElementById('recaptchaToken').value = token;
             });
         });

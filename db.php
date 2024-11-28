8148662f62e768738c7bfc0521302055fa61484a
JessaUngod <maeung00@gmail.com>
2024-11-28 23:12:16 +0800

    c

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php 
+session_start();
 
-
-// $con = mysqli_connect('localhost','root','','u510162695_sillon');s
+// $con = mysqli_connect('localhost','root','','u510162695_sillon');
 
 $con = mysqli_connect('127.0.0.1','u510162695_sillon','1Sillon_pass','u510162695_sillon');
 if(!$con){

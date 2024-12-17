4d78953193abbd1b24a5d00323ee23c7bdd59b88
JessaUngod <maeung00@gmail.com>
2024-12-18 05:36:51 +0800

    c

--- admin/admin_dash.php
+++ admin/admin_dash.php
@@ -226,8 +226,8 @@ $sql22 = "SELECT * FROM `employee_info`";
     var options = {
         chart: {
             type: 'bar',
-            height: 700, // Fixed height of 400px
-            width: '500%', // Responsive width, will take up full width of its container
+            height: 400, // Fixed height of 400px
+            width: '100%', // Responsive width, will take up full width of its container
             toolbar: {
                 show: false
             },

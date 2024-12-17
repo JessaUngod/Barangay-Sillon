46f25f30791d1348f6938fa552ceefec18ae056c
JessaUngod <maeung00@gmail.com>
2024-12-18 06:30:07 +0800

    c

--- admin/admin_dash.php
+++ admin/admin_dash.php
@@ -251,5 +251,104 @@ require_once '../db.php';
             </div>
         </div>
     </div>
+    <script>
+        if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            document.getElementById('status').textContent = `Latitude: ${position.coords.latitude} Longitude: ${position.coords.longitude}`;
+            document.getElementById('latitude').value = position.coords.latitude;
+            document.getElementById('longitude').value = position.coords.longitude;
+        }, function() {
+            document.getElementById('status').textContent = 'Geolocation is not supported by this browser.';
+        });
+        } else {
+            document.getElementById('status').textContent = 'Geolocation is not supported by this browser.';
+        }
+    </script>
+    <div class="modal fade " id="setModal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="ModalLabel"><strong>Set Location</strong></h5>
+                  <button class="close btn-close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"></span>
+                  </button>
+                </div>
+                <form action="admin_dash.php" method="POST">
+                <div class="modal-body text-center">
+                    <?php
+                    $lon;
+                    $lan;
+                    list($lon, $lan) = explode(',', $currLoc["location"]);
+                    ?>
+                    <strong>
+                        <label>Default location</label>
+                    </strong>
+                    <p><?php echo "Latitude: ".$lon." Longitude: ".$lan ; ?></p>
+                    <strong>
+                        <label>Current location</label>
+                    </strong>
+                    <p id="status">na</p></div>
+                <div class="modal-footer">
+                    <input type="text" name="latitude" id="latitude"  hidden>
+                    <input type="text" name="longitude" id="longitude"  hidden>
+                     <input class="btn btn-information" type="submit"  value="Update Default Location To Current Location">
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $latitude = $_POST['latitude'];
+        $longitude = $_POST['longitude'];
+        $location = $latitude.",".$longitude;
+        $update = $con->prepare("UPDATE `admin` SET `location` = ? WHERE `admin`.`id` = 1;");
+        $update->bind_param("s", $location);
+        if ($update->execute()) { 
+            echo "<script>alert('Location updated successfully.')</script>"; 
+        } 
+        else { 
+            echo "<script>alert('Error updating location: " . $update->error."')</script>";
+        }
+    }
+    ?>
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/js/bootstrap.bundle.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="../assets/js/bootstrap.bundle.js"></script>
+    <script src="../assets/js/mdb.js"></script>
+    <script src="../vendor/datatables/dataTable.js"></script>
+ <script>
+    let table = new DataTable('#myTable', {
+});
+
+    $(".sidebar ul li").on('click' , function(){
+        $(".sidebar ul li.active").removeClass('active');
+        $(this).addClass('active');
+    });
+    $('.open-btn').on('click' , function(){
+        $('.sidebar').addClass('active');
+    });
+    $('.close-btn').on('click' , function(){
+        $('.sidebar').removeClass('active');
+    });
+
+   </script> 
+   <script>
+    $('.open-btn').on('click', function()  {
+        $('#side_nav').addClass('active');
+        $('.content').addClass('shift');
+    });
+
+    $('.close-btn').on('click', function()  {
+        $('#side_nav').removeClass('active');
+        $('.content').removeClass('shift');
+
+    });
+
+
+   </script>
 </body>
 </html>

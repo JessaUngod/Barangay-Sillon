8b054c314066cf87f4dcfb915597a30c622eaab1
JessaUngod <maeung00@gmail.com>
2024-12-18 07:44:39 +0800

    c

--- admin/admin_dash.php
+++ admin/admin_dash.php
@@ -66,14 +66,14 @@ require_once '../db.php';
             border-radius: 0 0 15px 15px;
         }
 
-        .card-icon {
+        /* .card-icon {
             font-size: 3.5em;
             color: #ffffff;
             padding: 15px;
             border-radius: 50%;
             background-color: rgba(255, 255, 255, 0.2);
             box-shadow: 0 6px 15px rgba(0, 0, 0, 0.15);
-        }
+        } */
 
         .card-icon.bg-primary { background-color: #3498db; }
         .card-icon.bg-warning { background-color: #f39c12; }

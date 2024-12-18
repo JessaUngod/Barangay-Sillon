a608d8d564ee55f53725ef0173efa07f4c711f73
JessaUngod <maeung00@gmail.com>
2024-12-19 00:06:27 +0800

    c

--- admin/staff.php
+++ admin/staff.php
@@ -151,7 +151,7 @@ require_once'../db.php';
                         <div class="card-header">
                             <h6 class="m-0  fw-bold" style="color: #000;"><strong style="font-size: 30px;"> Staff  </strong><a class="btn btn-primary" href="add_staff.php" style="color: #fff;"><i class="fas fa-add fs-7" style="color: #fff;"></i><strong class="fs-7">  Add Staff</strong></a></h6>
                         </div>
-                        <div class="card-body" style="background-color: #f8f9fa; border-radius: 8px;">
+                        <div class="card-body" style="background-color:  #8e44ad; border-radius: 8px;">
     <div class="table-responsive">
         <table class="table table-bordered table-striped table-hover" id="myTable" width="100%" cellspacing="0">
             <thead style="font-size: 1em; color: #fff;">

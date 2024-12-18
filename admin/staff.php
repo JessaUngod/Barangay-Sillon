2b7c505253b84fededf6ab3ca4b101ff3eac9edb
JessaUngod <maeung00@gmail.com>
2024-12-19 02:13:56 +0800

    c

--- admin/staff.php
+++ admin/staff.php
@@ -203,8 +203,8 @@ require_once'../db.php';
                         </div>
                         <div class="card-body">
     <div class="table-responsive">
-        <table class="table table-bordered" id="myTable" width="100%" cellspacing="0">
-            <thead>
+        <table class="table" id="myTable" width="100%" cellspacing="0">
+            <thead class="table-light">
                 <tr>
                     <th>First Name</th>
                     <th>Middle Name</th>

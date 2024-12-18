40e7fb47b137bd9b868af13bc637128522188a70
JessaUngod <maeung00@gmail.com>
2024-12-19 02:00:29 +0800

    c

--- admin/staff.php
+++ admin/staff.php
@@ -134,18 +134,14 @@ require_once'../db.php';
  
 
                 <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-mdb-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-lg-inline  small " style="color: #000;">
-                               Hello,</span>
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-mdb-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-lg-inline small" style="color: #fff; font-family: 'Poppins', sans-serif; font-size: 30px;">Hello</span>
                                <span class="mr-2 d-lg-inline  small ">
                                 <img src="../uploads/<?php echo $row['img']; ?>" style="height: 40px; width:40px; border-radius:50%;" >
                                    
                                </span>
-                                <span class="mr-2 d-lg-inline  small fw-bold"  style="color: #000;">
-                                    <?php echo $row['fname']; ?>                                
-                                         </span>
-                                
+                               <span class="mr-2 d-lg-inline small" style="color: #fff; font-family: 'Poppins', sans-serif; font-size: 20px;;"><?php echo $row['fname']; ?></span>
+
                             </a>
 
                               <div class="dropdown-menu  shadow animated-grow-in px-4"

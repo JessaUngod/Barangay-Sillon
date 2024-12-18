0175e54335cf8c84a03ad7e12d0c80d6e30445b0
JessaUngod <maeung00@gmail.com>
2024-12-18 09:43:41 +0800

    c

--- admin/admin_dash.php
+++ admin/admin_dash.php
@@ -283,7 +283,7 @@ require_once '../db.php';
                     </div>
 
                     <!-- Total Employees Card -->
-                    <div class="col-md-3 mb-4">
+                    <div class="col-xl-2 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-body bg-success">
                                 <div class="row no-gutters align-items-center">
@@ -329,7 +329,7 @@ require_once '../db.php';
                     </div>
 
                     <!-- Total Absent Card -->
-                    <div class="col-md-3 mb-4">
+                    <div class="col-xl-2 col-md-6 mb-4">
                         <div class="card">
                             <div class="card-body bg-info">
                                 <div class="row no-gutters align-items-center">

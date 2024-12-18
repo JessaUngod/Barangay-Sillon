6f6bb4421a6e4ea42b354c8a367126766a0d7dfa
JessaUngod <maeung00@gmail.com>
2024-12-19 00:43:17 +0800

    c

--- admin/staff.php
+++ admin/staff.php
@@ -135,8 +135,10 @@ require_once'../db.php';
                     <div class="card mb-2 py-2 px-2">
                         <div class="row">
                             <div class="col-md-2">
-                                <a href="staff.php" class="btn form-control fw-bold btn-primary mb-1 mt-1 text-light">Staff</a>
-                            </div>
+                            <a href="staff.php" class="btn form-control fw-bold btn-primary mb-1 mt-1 text-light" 
+   style="background-color: #3498db; color: #fff; font-weight: bold; text-align: center; padding: 10px 15px; border-radius: 5px; display: inline-block; width: auto; text-decoration: none; transition: background-color 0.3s ease;">
+   Staff
+</a>                            </div>
                             <div class="col-md-2">
                                 <a href="admin.php" class="btn form-control fw-bold btn-secondary mb-1 mt-1 text-dark">Admin</a>
                             </div>

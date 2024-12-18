05c808494b483ba54d5affc029199f33d688f08a
JessaUngod <maeung00@gmail.com>
2024-12-18 09:58:58 +0800

    c

--- admin/admin_dash.php
+++ admin/admin_dash.php
@@ -239,7 +239,7 @@ require_once '../db.php';
                 <!-- Dashboard Stats Cards -->
                 <div class="row">
                     <!-- Admin Card -->
-                    <div class="col-xl-2 col-md-6 mb-4">
+                    <div class="col-xs-6">
                         <div class="card">
                             <div class="card-body bg-primary">
                                 <div class="row no-gutters align-items-center">
